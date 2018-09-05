f7ed43bebaf00ddfaf586273685be5a94d05ac79
mlizchap <mlizchap@gmail.com>
2018-09-05 13:26:01 -0400

    changed styling

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+
 import Todo from '../Todo/Todo'
+import './TodoList.scss';
 
 class TodoList extends Component {
 
@@ -7,7 +9,13 @@ class TodoList extends Component {
         //return <div />
         return this.props.todos.map((i) => {
             return (
-                <Todo key={i.name} name={i.name} completed={i.completed} {...this.props} />
+                <Todo 
+                    className="component-todolist"
+                    key={i.name} 
+                    name={i.name} 
+                    completed={i.completed} 
+                    {...this.props} 
+                />
             )
         })
     }
